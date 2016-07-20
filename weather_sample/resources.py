869dcab726b468ed0d52a3d61b08e581932a6783
David Soler <davids@eguestcontrols.com>
2016-07-20 09:22:19 -0400

    Renamed type to recommendation_type to not shadow type variable

--- weather_sample/resources.py
+++ weather_sample/resources.py
@@ -1,15 +1,15 @@
 class Recommendation:
 
-    def __init__(self, title, description, url, type):
+    def __init__(self, title, description, url, recommendation_type):
         self.title = title
         self.description = description
         self.url = url
-        self.type = type
+        self.recommendation_type = recommendation_type
 
     def serialize(self):
         return {
             "Title": self.title,
             "Description": self.description,
             "Url": self.url,
-            "Type": self.type
+            "Type": self.recommendation_type
         }
