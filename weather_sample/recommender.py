b07df40c0eca0af21d46b704bf89a373a724a688
David Soler <davids@eguestcontrols.com>
2016-07-20 09:01:24 -0400

    Added more comments

--- weather_sample/recommender.py
+++ weather_sample/recommender.py
@@ -19,7 +19,7 @@ class Recommender:
     def __get_netflix_recommendation__(self):
         shows = self.netflix_service.get_netflix_shows()
 
-        # TODO actually create a recommendation algorithm
+        # TODO actually create a recommendation algorithm for shows
         show = random.choice(shows)
 
         info = self.netflix_service.get_show_information(show)
@@ -39,6 +39,7 @@ class Recommender:
         if len(events) == 0:
             return self.__get_netflix_recommendation__()
 
+        # TODO actually create a recommendation algorithm for events
         event = random.choice(events)
 
         recommendation = Recommendation(
@@ -53,6 +54,9 @@ class Recommender:
     def get_recommendation(self, latitude, longitude):
         hourly_weather = self.weather_service.get_hourly_weather(latitude, longitude)
 
+        # TODO add more fine grained detail as to why recommendation was given
+        # i.e. 'It's too hot outside!' or 'It's raining!'
+        
         if any(self.__is_stay_in_weather__(hour) for hour in hourly_weather['data']):
             return self.__get_netflix_recommendation__()
         else:
