a90c411634e1f111bec9500b7e0626f8c86e3295
David Soler <davids@eguestcontrols.com>
2016-07-22 15:24:16 -0400

    Don't know why I was using a POST... also cast to float not int

--- recommendation/main.py
+++ recommendation/main.py
@@ -1,4 +1,3 @@
-
 from recommendation.services import WeatherService, NetflixRouletteService, EventbriteService
 from recommendation.recommender import Recommender
 
@@ -6,17 +5,22 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+recommender = Recommender(
+    WeatherService(),
+    EventbriteService(),
+    NetflixRouletteService()
+)
+
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
 
-@app.route('/recommendations/', methods=['POST'])
+@app.route('/recommendations/', methods=['GET'])
 def choice():
-
-    latitude = int(request.form['latitude'])
-    longitude = int(request.form['longitude'])
+    latitude = float(request.args.get('latitude'))
+    longitude = float(request.args.get('longitude'))
 
     # This should return something prettier
     if latitude < -90 or latitude > 90:
@@ -24,12 +28,8 @@ def choice():
     if longitude < -180 or longitude > 180:
         return "Invalid longitude range"
 
-    recommender = Recommender(WeatherService(),
-                              EventbriteService(),
-                              NetflixRouletteService())
-
     return render_template("recommendations/choice.html",
-                           recommendation=recommender.get_recommendation(request.form['latitude'], request.form['longitude']))
+                           recommendation=recommender.get_recommendation(latitude, longitude))
 
 
 if __name__ == "__main__":
